78f6a22cf1751539800beaf2ff68c816bde147de
Seth <itz.saga@gmail.com>
2018-01-06 21:32:20 -0600

    Add header component

--- src/container/App.js
+++ src/container/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import Header from '../presentational/header';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <Header />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.

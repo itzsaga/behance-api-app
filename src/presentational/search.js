d5fb7dd74e62600afdbe58edcfaa10dcee092f06
Seth <itz.saga@gmail.com>
2018-01-06 21:39:47 -0600

    Create search input SFC

---
+++ src/presentational/search.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Search = ({ handleChange, handleSearch, searchTerm }) => (
+  <div id="search">
+    <div className="field has-addons has-addons-centered">
+      <div className="control has-icons-left">
+        <input
+          className="input"
+          type="text"
+          placeholder="Search Behance Users"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="control">
+        <a className="button is-primary" onClick={handleSearch} >
+          Search
+        </a>
+      </div>
+    </div>
+  </div>
+);
+
+export default Search;

a7ec61c0ba7a86e07844f2226891a3345054105d
Seth <itz.saga@gmail.com>
2018-01-06 21:31:54 -0600

    Import bulma

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+// Bulma & any other CSS
+import 'bulma/css/bulma.css';
+
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Components
 import App from './container/App';
+
+// Service Worker
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root')); // eslint-disable-line

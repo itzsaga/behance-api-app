08e4cac7718ef089966307d55a97c1a0d2c90acc
Seth <itz.saga@gmail.com>
2018-01-07 17:19:13 -0600

    Setup helper file

---
+++ src/helper/apiCalls.js
@@ -0,0 +1,27 @@
+const fetchUserInfo = (id) => {
+
+};
+
+const fetchUserProjects = (id) => {
+
+};
+
+const fetchUserFollowers = (id) => {
+
+};
+
+const fetchUserFollowings = (id) => {
+
+};
+
+const fetchWorkExperience = (id) => {
+
+};
+
+export default {
+  fetchUserInfo,
+  fetchWorkExperience,
+  fetchUserProjects,
+  fetchUserFollowers,
+  fetchUserFollowings
+};

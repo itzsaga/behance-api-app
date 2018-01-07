bcde8aef7ecec2809de6becf66ea00bcdfe132e2
Seth <itz.saga@gmail.com>
2018-01-06 21:32:12 -0600

    Starting header component

---
+++ src/presentational/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <div id="title">
+    <p className="title">A Behance Viewer!</p>
+    <p className="subtitle">Coded by <a href="http://sethalexander.me" target="_blank" rel="noopener noreferrer">Seth Alexander</a></p>
+  </div>
+);
+
+export default Header;

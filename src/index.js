a287f540262d58f462eada39f28c5c46d7cd75ec
Seth <itz.saga@gmail.com>
2018-01-06 23:58:08 -0600

    Start of react-router implementation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'bulma/css/bulma.css';
 // React
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Router, Route } from 'react-router-dom';
 
 // Components
 import App from './container/App';
@@ -11,5 +12,11 @@ import App from './container/App';
 // Service Worker
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root')); // eslint-disable-line
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root') // eslint-disable-line
+);
+
 registerServiceWorker();

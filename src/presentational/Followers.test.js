66c730d7cef6227773c554ea21d9ecd526d868ba
Seth <itz.saga@gmail.com>
2018-01-08 00:10:54 -0600

    Starter test files

---
+++ src/presentational/Followers.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Followers from './Followers';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Followers />, div);
+});

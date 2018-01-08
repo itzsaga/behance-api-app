743c0e96f506cebb81cb4f431f21034666bf9b47
Seth <itz.saga@gmail.com>
2018-01-08 00:05:58 -0600

    Project list styling

--- src/presentational/Projects.js
+++ src/presentational/Projects.js
@@ -3,16 +3,18 @@ import React from 'react';
 const Projects = ({ projects }) => {
   const trimmedList = projects.slice(0, 10);
   const projectList = trimmedList.map(project =>
-    (<div className="media" key={project.id}>
-      <div className="media-left">
-        <figure className="image is-128x128">
-          <img src={project.covers[404]} alt="Project cover" />
-        </figure>
-      </div>
-      <div className="media-content">
-        <p className="title is-4">{project.name}</p>
-        <p className="subtitle is-6">Viewed: {project.stats.views} times</p>
-        <a href={project.url} className="button is-link is-rounded">More info...</a>
+    (<div className="box">
+      <div className="media" key={project.id}>
+        <div className="media-left">
+          <figure className="image is-128x128">
+            <img src={project.covers[404]} alt="Project cover" />
+          </figure>
+        </div>
+        <div className="media-content">
+          <p className="title is-4">{project.name}</p>
+          <p className="subtitle is-6">Viewed: {project.stats.views} times</p>
+          <a href={project.url} className="button is-link is-rounded">More info...</a>
+        </div>
       </div>
     </div>));
   return (

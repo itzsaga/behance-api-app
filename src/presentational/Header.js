b162c5addb4d2dc6b56675e52f7cf7242d048e49
Seth <itz.saga@gmail.com>
2018-01-07 23:40:01 -0600

    Center text

--- src/presentational/Header.js
+++ src/presentational/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
   <div id="title">
-    <p className="title">Search For A Behance User</p>
+    <p className="title has-text-centered">Search For A Behance User</p>
   </div>
 );
 

1da32eb1ed0dc1e7c9b70647094beff63893ce2f
Seth <itz.saga@gmail.com>
2018-01-07 23:28:40 -0600

    Redo tiles with columns

--- src/presentational/Profile.js
+++ src/presentational/Profile.js
@@ -6,48 +6,34 @@ import Following from './Following';
 
 const Profile = ({ projects, workExperience, followers, following, user }) => (
   <div className="container is-fluid">
-    <div className="tile is-ancestor">
-      <div className="tile is-vertical is-4">
-        <div className="tile is-parent is-vertical">
-          <div className="tile is-child">
-            <div className="card">
-              <div className="card-image">
-                <figure className="image is-square">
-                  <img src={user.images[276]} alt="Profile" />
-                </figure>
-              </div>
-              <div className="card-content">
-                <div className="content">
-                  <p className="title is-4">{user.display_name}</p>
-                  <p className="subtitle is-6">{user.company}<br />
-                    {user.location}<br />
-                    <a href={user.website}>{user.website}</a>
-                  </p>
-                </div>
-                <div className="content">
-                  {user.sections.Description}
-                </div>
-                <a href={user.url} className="button is-link is-rounded">More info on Behance</a>
-              </div>
+    <div className="columns">
+      <div className="column is-4">
+        <div className="card">
+          <div className="card-image">
+            <figure className="image is-square">
+              <img src={user.images[276]} alt="Profile" />
+            </figure>
+          </div>
+          <div className="card-content">
+            <div className="content">
+              <p className="title is-4">{user.display_name}</p>
+              <p className="subtitle is-6">{user.company}<br />
+                {user.location}<br />
+                <a href={user.website}>{user.website}</a>
+              </p>
             </div>
+            <div className="content">
+              {user.sections.Description}
+            </div>
+            <a href={user.url} className="button is-link is-rounded">More info on Behance</a>
           </div>
-          <div className="tile is-child">
-            <WorkExperience workExperience={workExperience} />
-          </div>
-          <div className="tile is-child">
-            <Followers followers={followers} />
-          </div>
-          <div className="tile is-child">
-            <Following following={following} />
-          </div>
+          <WorkExperience workExperience={workExperience} />
+          <Followers followers={followers} />
+          <Following following={following} />
         </div>
       </div>
-      <div className="tile is-vertical">
-        <div className="tile is-parent is-vertical">
-          <div className="tile is-child">
-            <Projects projects={projects} />
-          </div>
-        </div>
+      <div className="column">
+        <Projects projects={projects} />
       </div>
     </div>
   </div>

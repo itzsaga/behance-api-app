638223ca7e9e2c566935b5fd1fd4b6d7516c0c22
Seth <itz.saga@gmail.com>
2018-01-10 00:20:40 -0600

    It renders without crashing

--- src/__tests__/WorkExperience.test.js
+++ src/__tests__/WorkExperience.test.js
@@ -3,5 +3,12 @@ import { shallow } from 'enzyme';
 import WorkExperience from '../presentational/WorkExperience';
 
 it('renders without crashing', () => {
-  shallow(<WorkExperience />);
+  const workExperience = [{
+    'position': 'Digital Artist',
+    'start_date': '05-2009',
+    'organization': 'Mike Campau Digital Imagery LLC',
+    'location': 'MI, USA'
+  }];
+
+  shallow(<WorkExperience workExperience={workExperience} />);
 });

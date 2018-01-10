eebd7b06af3ad2d16d8ed12fca69824e0fefabd0
Seth <itz.saga@gmail.com>
2018-01-09 22:38:25 -0600

    Add key to iterator

--- src/presentational/WorkExperience.js
+++ src/presentational/WorkExperience.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const WorkExperience = ({ workExperience }) => {
   const workList = workExperience.map(work => (
-    <div className="box">
+    <div className="box" key={work.start_date}>
       <div className="content">
         <p className="title is-4">{work.organization}</p>
         <p className="subtitle is-6">{work.position}<br />

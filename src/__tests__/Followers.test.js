5b0a39abb02b7da15c2f50f649747ca8e12a5b5d
Seth <itz.saga@gmail.com>
2018-01-10 00:42:03 -0600

    Trimmed excess test data

--- src/__tests__/Followers.test.js
+++ src/__tests__/Followers.test.js
@@ -5,33 +5,11 @@ import Followers from '../presentational/Followers';
 it('renders without crashing', () => {
   const followers = [{
     id: 5724657,
-    first_name: 'eris',
-    last_name: 'eris',
-    username: 'eris6027',
-    city: 'Shanghai',
-    state: '',
-    country: 'China',
     location: 'Shanghai, China',
-    company: '',
-    occupation: '',
-    created_on: 1398408159,
+    company: 'The Moon',
+    occupation: 'Artist',
     url: 'https://www.behance.net/eris6027',
-    images: {
-      50: 'https://a5.behance.net/2604992a5a6a3839cf279cf9beb3cd63d5ce5354/img/profile/no-image-50.jpg?cb=264615658',
-      115: 'https://a5.behance.net/2604992a5a6a3839cf279cf9beb3cd63d5ce5354/img/profile/no-image-115.jpg?cb=264615658',
-      138: 'https://a5.behance.net/2604992a5a6a3839cf279cf9beb3cd63d5ce5354/img/profile/no-image-138.jpg?cb=264615658'
-    },
-    display_name: 'eris eris',
-    fields: [],
-    has_default_image: 1,
-    website: '',
-    stats: {
-      followers: 2,
-      following: 271,
-      appreciations: 0,
-      views: 0,
-      comments: 0
-    }
+    display_name: 'eris eris'
   }];
 
   shallow(<Followers followers={followers} />);
--- src/__tests__/SearchResults.test.js
+++ src/__tests__/SearchResults.test.js
@@ -5,43 +5,12 @@ import SearchResults from '../presentational/SearchResults';
 it('renders without crashing', () => {
   const searchResults = [{
     id: 433679,
-    first_name: 'Brosmind',
-    last_name: '®',
-    username: 'brosmind',
-    city: 'Barcelona',
-    state: '',
-    country: 'Spain',
-    location: 'Barcelona, Spain',
     company: 'Brosmind SL',
-    occupation: '',
-    created_on: 1304347499,
-    url: 'https://www.behance.net/brosmind',
     images: {
-      50: 'https://mir-s3-cdn-cf.behance.net/user/50/433679.54668a4bf09f6.jpg',
-      100: 'https://mir-s3-cdn-cf.behance.net/user/100/433679.54668a4bf09f6.jpg',
-      115: 'https://mir-s3-cdn-cf.behance.net/user/115/433679.54668a4bf09f6.jpg',
-      138: 'https://mir-s3-cdn-cf.behance.net/user/138/433679.54668a4bf09f6.jpg',
-      230: 'https://mir-s3-cdn-cf.behance.net/user/230/433679.54668a4bf09f6.jpg',
-      276: 'https://mir-s3-cdn-cf.behance.net/user/276/433679.54668a4bf09f6.jpg'
+      138: 'https://mir-s3-cdn-cf.behance.net/user/138/433679.54668a4bf09f6.jpg'
     },
-    display_name: 'Brosmind ®',
-    fields: [
-      'Illustration',
-      'Character Design',
-      'Advertising'
-    ],
-    has_default_image: 0,
-    website: 'www.brosmind.com',
-    stats: {
-      followers: 23770,
-      following: 15,
-      appreciations: 37985,
-      views: 484739,
-      comments: 1
-    }
+    display_name: 'Brosmind ®'
   }];
-
-  const setViewing = () => {};
 
   shallow(<SearchResults searchResults={searchResults} />);
 });
